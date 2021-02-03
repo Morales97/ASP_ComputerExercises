bddf48eff628b7afabb05f619dd11be7fd1dd789
Morales97 <danimoralesbrotons@gmail.com>
2021-02-03 14:09:41 +0100

    .

--- Project 1/main.m
+++ Project 1/main.m
@@ -2,7 +2,7 @@
 [z,fs] = audioread('EQ2401project1data2021.wav');
 
 %% FIR filter
-[shatfir, thetahatfir] = p1_firw(z, z(1:8000), 15, 10);
+[shatfir, thetahatfir] = p1_firw(z, z(1:8000), 50, 10);
 
 %% Non-Causal filter
-[shatnc, numnc, dennc] = p1_ncw(z, z(1:8000), 5, 20);
+[shatnc, numnc, dennc] = p1_ncw(z, z(1:8000), 20, 10);
