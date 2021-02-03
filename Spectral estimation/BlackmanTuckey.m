3f99ed17f8c8737e322504fade3c90f1877acbd3
Morales97 <danimoralesbrotons@gmail.com>
2021-02-03 13:32:58 +0100

    Update BlackmanTuckey.m

--- Spectral estimation/BlackmanTuckey.m
+++ Spectral estimation/BlackmanTuckey.m
@@ -1,4 +1,4 @@
-function [BT_spectrum] = BlackmanTuckey(y, window)
+function [w, BT_spectrum] = BlackmanTuckey(y)
     % Blackman Tuckey spectral estimation method
     %
     % P(nu) = FT{r(k)·w(k)}
@@ -6,16 +6,19 @@ function [BT_spectrum] = BlackmanTuckey(y, window)
     % 
     
     % Get ACF and multiply with window 
+    M = 40;
+    N = 1024;
+    window = hamming(2*M+1);
     K = length(y);
-    N = length(window);
-    M = ceil(N/2);
-    r_y = zeros(M, 1);
-    for k = 0:M-1
+    r_y = zeros(M+1, 1);
+    for k = 0:M
         seq1 = y(1:K-k);
         seq2 = y(k+1:K);
         r_y(k+1) = sum(seq1 .* seq2);
     end
     r_y = r_y/K;
-    r_y = [flip(r_y(2:M)); r_y];
+    r_y = [flip(r_y(2:M+1)); r_y];
     
-    BT_spectrum = 20 * log(abs(fft(r_y .* window, 1024)));
+    BT_spectrum = abs(fft(r_y .* window, N));
+    w = linspace(0,pi,N/2);
+    plot(w, BT_spectrum(1:N/2))
